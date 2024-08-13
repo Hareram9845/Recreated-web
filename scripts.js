f2e81197b87d16e518598651ece319e33ddce73c
Hareram9845 <165174986+Hareram9845@users.noreply.github.com>
2024-08-13 21:37:14 +0545

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,12 @@
+// scripts.js
+
+// Adding a simple functionality: Smooth scroll to sections
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
